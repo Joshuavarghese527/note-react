bec5ce526490b31523288421cf8156db77f470d3
Joshuavarghese527 <joshuavarghese527@gmail.com>
2017-06-05 20:21:07 -0400

    Nav Component

---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  render() { //render method that will turn JSX into HTML
+     return (
+      <div>
+        Nav Component
+      </div>
+    );
+  }
+}
+
+export default Nav;
